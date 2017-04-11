d65b5faf86d5347c13c6d10c55a2e1700fe21f93
mazong1123 <mazong1123@gmail.com>
2017-04-11 23:43:28 +0800

    Initial setup.

---
+++ SharpSync/SharpSync/Options.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpSync
+{
+    public class Options
+    {
+        public static void PrintVersion()
+        {
+        }
+        public static void PrintUsage()
+        {
+            PrintVersion();
+
+            Console.WriteLine();
+            Console.WriteLine("SharpSync is a cross-platform file transfer program inspired by rsync.");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SharpSync/SharpSync/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SharpSync
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Options.PrintUsage();
+
+                return;
+            }
+        }
+    }
+}
